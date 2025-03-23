3cae467dbc8775d62a6c73cbd8baa7957db2f6b1
namangarg2002 <namangarg20021128@gmail.com>
2025-03-23 11:01:51 +0530

    updated changes

--- techtrendz/src/App.jsx
+++ techtrendz/src/App.jsx
@@ -5,9 +5,15 @@ import Category from './components/Category/Category'
 import Category2 from './components/Category/Category2'
 import Services from './components/Services/Services'
 import Banner from './components/Banner/Banner'
-
-import headphone from './assets/hero/headphone.png'
 import Products from './components/Products/Products'
+import Blog from './components/Blogs/Blogs'
+import Partners from './components/Partners/Partners'
+
+// Import images
+import headphone from './assets/hero/headphone.png'
+import smartwatch2 from './assets/category/smartwatch2-removebg-preview.png'
+import Footer from './components/Footer/Footer'
+import Popup from './components/Popup/Popup'
 
 const BannerData = {
   discount: "30% OFF",
@@ -20,6 +26,17 @@ const BannerData = {
   bgColor: "#f42c37",
 };
 
+const BannerData2 = {
+  discount: "30% OFF",
+  title: "Happy Hours",
+  date: "14 Jan to 28 Jan",
+  image: smartwatch2,
+  title2: "Smart Solo",
+  title3: "Winter Sale",
+  title4: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
+  bgColor: "#2dcc6f",
+};
+
 const App = () => {
   return (
     <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden'>
@@ -30,6 +47,11 @@ const App = () => {
       <Services/>
       <Banner data={BannerData}/>
       <Products/>
+      <Banner data={BannerData2}/>
+      <Blog/>
+      <Partners/>
+      <Footer/>
+      <Popup/>
     </div>
   )
 }
